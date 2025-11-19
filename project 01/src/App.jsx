52c14cdf9f7c6f47446a746e9bc9ec3750d910ac
Ananya <ananyabera@gmail.com>
2025-11-19 23:41:20 +0530

    new component added

--- project 01/src/App.jsx
+++ project 01/src/App.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import './App.css'
 
 const App = () => {
   return (
-    <div>
-      App
+     <div className='parent'>
+      <div className="card">
+        <div className="main">
+        <div className="top">
+            <p>19 Nov 2025</p>
+            <button>save</button>
+        </div>
+
+        <div className="center">
+          <p>Amazon</p>
+          <span>Senior UI/UX Designer
+            <img src="https://i.pinimg.com/originals/01/ca/da/01cada77a0a7d326d85b7969fe26a728.jpg" alt="external-link" />
+          </span>
+          .
+        </div>
+        </div>
+
+        <div className="bottom">
+
+        </div>
+
+     </div> 
     </div>
   )
 }
