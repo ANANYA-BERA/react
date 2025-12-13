f1f716a7d9144b74ab3425daaeb6fa2a8ab8ec14
Ananya <ananyabera103@gmail.com>
2025-12-13 21:28:27 +0530

    day09

--- UI-project/src/components/section1/Navbar.jsx
+++ UI-project/src/components/section1/Navbar.jsx
@@ -4,7 +4,7 @@ const navbar = () => {
   return (
     <div>
       <div className='flex items-center justify-between px-10 py-5'>
-      <h4 className='bg-black text-white uppercase px-5 py-1 rounded-full text-xm '>Target Audience</h4>
+      <h4 className='bg-black text-white uppercase px-5 py-1 rounded-4xl text-xm '>Target Audience</h4>
       <button className='bg-gray-200 uppercase px-5 py-1 rounded-full tracking-widest text-xm'>Digital Banking Platform</button>
     </div>
     </div>
